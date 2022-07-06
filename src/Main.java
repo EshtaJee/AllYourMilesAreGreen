f0839ae00b083eab12f7280de3277ed180a957c7
EshtaJee <Logrus-10@yandex.ru>
2022-07-07 02:38:26 +0300

    FixForCalculate

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 876543;
+        int miles = service.calculate(price);
+
         System.out.println("Добро пожаловать в авиакомпанию \"Верхом на Метле\".");
 
-        int checkForTicket = 566563457; // цена билета
-        int checkForOneMile = 20; // стоит одна миля
-
-        if (checkForTicket > 0) {
+        if (miles > 0) {
             System.out.println();
-            System.out.println("Ого го! Стоимость Вашего билета составляет " + (checkForTicket) + " рублей");
-            System.out.println("Хм... За это мы начислим Вам, скажем... " + (checkForTicket / checkForOneMile) + " бонусных миль");
-            System.out.println("Как Вам такое, Илон Маск?");
+            System.out.println("Ого го! Стоимость Вашего билета составляет " + (price) + " рублей. \nХм... За это мы начислим Вам, скажем... " + (miles) + " бонусных миль. \nКак Вам такое, Илон Маск?");
             System.out.println();
             System.out.println("Спасибо. Наши метлы самые удобные!");
         } else {
             System.out.println();
-            System.out.println("В смысле цена билета " + (checkForTicket) + " рублей?");
-            System.out.println("Что ж... Сделать бы Вам " + (checkForTicket / checkForOneMile) + " бонусных миль, чтоб все было честно! \nНо мы ребята добрые, поэтому просто ничего Вам не начислим...");
-            System.out.println("Как Вам такое, Илон Маск?");
+            System.out.println("В смысле цена билета " + (price) + " рублей?! \nЧто ж... Сделать бы Вам " + (miles) + " бонусных миль, чтоб все было честно! \nНо мы ребята добрые, поэтому просто ничего Вам не начислим...");
             System.out.println();
             System.out.println("Спасибо. Наши метлы самые удобные! Но не для Вас!");
         }
-
     }
 }
